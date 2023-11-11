3de31e722d2c47398ac9f989243b28d2528e54f5
Ermo Wei <nosyndicate@gmail.com>
2023-11-10 23:18:07 -0800

    some refactoring

--- llm/attention.py
+++ llm/attention.py
@@ -1,10 +1,10 @@
 from torch import nn, Tensor, LongTensor
 from llm.rotary_embedding import RotaryEmbedding, apply_rotary_pos_emb
-from typing import Optional
 
 import torch 
 from torch.nn import functional as F
 import math
+from flash_attn import flash_attn_func
 
 
 class Attention(nn.Module):
@@ -21,7 +21,7 @@ class Attention(nn.Module):
         self,
         hidden_size: int,
         num_heads: int,
-        num_key_value_heads: Optional[int] = None,
+        num_key_value_heads: int | None = None,
         attention_bias: bool = False,
     ):
         self.hidden_size = hidden_size
@@ -57,7 +57,7 @@ class Attention(nn.Module):
     def forward(
         self,
         hidden_states: Tensor,
-        position_ids: Optional[LongTensor] = None,
+        position_ids: LongTensor | None = None,
     ):
         batch_size, q_len, _ = hidden_states.size()
 
@@ -85,8 +85,6 @@ class Attention(nn.Module):
             query_states, key_states, cos, sin, position_ids
         )
 
-
-
 class CausalSelfAttention(nn.Module):
 
     def __init__(self, config):
@@ -102,6 +100,7 @@ class CausalSelfAttention(nn.Module):
         self.n_head = config.n_head
         self.n_embd = config.n_embd
         self.dropout = config.dropout
+        self.flash_attention = config.flash_attention
         # flash attention make GPU go brrrrr but support is only in PyTorch >= 2.0
         self.flash = hasattr(torch.nn.functional, 'scaled_dot_product_attention')
         if not self.flash:
@@ -115,22 +114,32 @@ class CausalSelfAttention(nn.Module):
 
         # calculate query, key, values for all heads in batch and move head forward to be the batch dim
         q, k, v  = self.c_attn(x).split(self.n_embd, dim=2)
-        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        k = k.view(B, T, self.n_head, C // self.n_head)  # (B, T, nh, hs) 
+        q = q.view(B, T, self.n_head, C // self.n_head)  # (B, T, nh, hs) 
+        v = v.view(B, T, self.n_head, C // self.n_head)  # (B, T, nh, hs) 
 
-        # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
-        if self.flash:
-            # efficient attention using Flash Attention CUDA kernels
-            y = torch.nn.functional.scaled_dot_product_attention(q, k, v, attn_mask=None, dropout_p=self.dropout if self.training else 0, is_causal=True)
+        # first try flash attention
+        if self.flash_attention:
+            scale = 1.0 / math.sqrt(k.size(-1))
+            y = flash_attn_func(q, k, v, dropout_p=self.dropout if self.training else 0, softmax_scale=scale, causal=True)
+            y = y.contiguous().view(B, T, C)
         else:
-            # manual implementation of attention
-            att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
-            att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
-            att = F.softmax(att, dim=-1)
-            att = self.attn_dropout(att)
-            y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
-        y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
+            # if not using flash attention, use original implementation from nanogpt
+            # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
+            q = q.transpose(1, 2) # (B, nh, T, hs)
+            k = k.transpose(1, 2) # (B, nh, T, hs)
+            v = v.transpose(1, 2) # (B, nh, T, hs)
+            if self.flash:
+                # efficient attention using Flash Attention CUDA kernels
+                y = torch.nn.functional.scaled_dot_product_attention(q, k, v, attn_mask=None, dropout_p=self.dropout if self.training else 0, is_causal=True)
+            else:
+                # manual implementation of attention
+                att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+                att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
+                att = F.softmax(att, dim=-1)
+                att = self.attn_dropout(att)
+                y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+            y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
 
         # output projection
         y = self.resid_dropout(self.c_proj(y))
