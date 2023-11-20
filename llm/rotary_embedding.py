d194b2610c35544a65c28cbb405592646cd86b46
Ermo Wei <nosyndicate@gmail.com>
2023-11-19 16:43:42 -0800

    clean up some rope embedding stuff

--- llm/rotary_embedding.py
+++ llm/rotary_embedding.py
@@ -1,90 +1,6 @@
 import torch
 from torch import nn
 
-# class RotaryEmbedding(nn.Module):
-#     def __init__(
-#         self,
-#         dim: int,
-#         max_position_embeddings: int = 2048,
-#         base: int = 10000,
-#         device=None,
-#     ):
-#         super().__init__()
-#         inv_freq = 1.0 / (base ** (torch.arange(0, dim, 2).float().to(device) / dim))
-#         self.register_buffer("inv_freq", inv_freq)
-
-#         # Build here to make `torch.jit.trace` work.
-#         self.max_seq_len_cached = max_position_embeddings
-#         t = torch.arange(
-#             self.max_seq_len_cached,
-#             device=self.inv_freq.device,
-#             dtype=self.inv_freq.dtype,
-#         )
-#         freqs = torch.einsum("i,j->ij", t, self.inv_freq)
-#         # Different from paper, but it uses a different permutation in order to obtain the same calculation
-#         emb = torch.cat((freqs, freqs), dim=-1)
-#         self.register_buffer(
-#             "cos_cached", emb.cos()[None, None, :, :], persistent=False
-#         )
-#         self.register_buffer(
-#             "sin_cached", emb.sin()[None, None, :, :], persistent=False
-#         )
-
-#     def forward(self, x: torch.Tensor, seq_len: int | None = None):
-#         """
-#         x should of size  [batch_size, num_attention_heads, seq_len, head_size]
-#         """
-#         # This `if` block is unlikely to be run after we build sin/cos in `__init__`. Keep the logic here just in case.
-#         if seq_len > self.max_seq_len_cached:
-#             self.max_seq_len_cached = seq_len
-#             t = torch.arange(
-#                 self.max_seq_len_cached, device=x.device, dtype=self.inv_freq.dtype
-#             )
-#             freqs = torch.einsum("i,j->ij", t, self.inv_freq)
-#             # Different from paper, but it uses a different permutation in order to obtain the same calculation
-#             emb = torch.cat((freqs, freqs), dim=-1).to(x.device)
-#             self.register_buffer(
-#                 "cos_cached", emb.cos()[None, None, :, :], persistent=False
-#             )
-#             self.register_buffer(
-#                 "sin_cached", emb.sin()[None, None, :, :], persistent=False
-#             )
-#         return (
-#             self.cos_cached[:, :, :seq_len, ...].to(dtype=x.dtype),
-#             self.sin_cached[:, :, :seq_len, ...].to(dtype=x.dtype),
-#         )
-
-
-# def rotate_half(x):
-#     """
-#     Rotates half the hidden dims of the input.
-
-#     Transform a embedding vector of
-#         (x_1, x_2, x_3, ..., x_{d-1}, x_d)
-#     into
-#         (-x_2, x_1, -x_4, ..., -x_d, x_{d-1})
-#     """
-#     x1 = x[..., : x.shape[-1] // 2]
-#     x2 = x[..., x.shape[-1] // 2 :]
-#     return torch.cat((-x2, x1), dim=-1)
-
-
-# def apply_rotary_pos_emb(q, k, cos, sin, position_ids):
-#     """
-#     Apply position embedding on query and key embeddings
-#     Equation (34) of https://arxiv.org/pdf/2104.09864.pdf
-#     """
-#     # The first two dimensions of cos and sin are always 1, so we can `squeeze` them.
-#     cos = cos.squeeze(1).squeeze(0)  # [seq_len, dim]
-#     sin = sin.squeeze(1).squeeze(0)  # [seq_len, dim]
-#     cos = cos[position_ids].unsqueeze(1)  # [bs, 1, seq_len, dim]
-#     sin = sin[position_ids].unsqueeze(1)  # [bs, 1, seq_len, dim]
-#     q_embed = (q * cos) + (rotate_half(q) * sin)
-#     k_embed = (k * cos) + (rotate_half(k) * sin)
-#     return q_embed, k_embed
-
-RopeEmbedding = tuple[torch.Tensor, torch.Tensor]
-
 
 def build_rope(base: int, d: int, max_seq_len: int, ) -> tuple[torch.Tensor, torch.Tensor]:
     """
@@ -102,7 +18,6 @@ def build_rope(base: int, d: int, max_seq_len: int, ) -> tuple[torch.Tensor, tor
     Returns:
         cos: Cosine part of the position embedding 
         sin: Sine part of the position embedding
-
     """
     # Compute theta, this theta is universal for all positions
     theta = 1.0 / (base ** (torch.arange(0, d, 2).float() / d))
@@ -154,6 +69,6 @@ def apply_rotary_embedding(
         cos: (T, head_dim)
         sin: (T, head_dim)
     """
-
+    # Apply the position embedding on the last two dimensions
     roped = (tok_embedding * cos) + (rotate_half(tok_embedding) * sin)
     return roped.type_as(tok_embedding)
